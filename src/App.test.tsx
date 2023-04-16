aded437455a4826a8d9628988618c34e04332b15
Silambarasan R <silambu1821944@gmail.com>
2023-04-17 04:46:59 +0530

    Meta detail changes

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Tic Tac Toe', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Tic Tac Toe/i);
   expect(linkElement).toBeInTheDocument();
 });
